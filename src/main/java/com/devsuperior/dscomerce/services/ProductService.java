a4a5fcea44265093e95442f5722f4ebd73cfd60b
Marcelo Cortez <mhcortez@gmail.com>
2026-02-03 10:37:50 -0300

    Buscar produtos paginados

--- src/main/java/com/devsuperior/dscomerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscomerce/services/ProductService.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscomerce.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,16 +19,15 @@ public class ProductService {
     private ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public ProductDTO findById(Long id) {
-        // Lógica para buscar o produto pelo ID
-        /*Optional<Product> result = repository.findById(id);
-        Product product = result.get();
-        ProductDTO dto = new ProductDTO(product);
-        return dto; */
-
-        //Forma refatorada
+    public ProductDTO findById(Long id) {       
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {       
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 
 }
